5c2745c4e1e8d2daa850ee13ea0cd8a2a1b11be6
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2026-01-01 22:10:01 +0100

    ci: Added Deploy workflow

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
+// Detect if running in GitHub Actions
+const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig = {
+  basePath: isGitHubActions ? "/xenia-mirror" : "",
+  output: "export", // Enable static export for GitHub Pages
   images: {
     remotePatterns: [
       {
